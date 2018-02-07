400c02e9fdcb910a5f6148b33a05e569e457b538
marcusvlc <marcus.costa@ccc.ufcg.edu.br>
2018-02-07 12:05:13 -0300

    teste na classe startup

--- src/main/java/br/com/lab2spring/ApiwebApplication.java
+++ src/main/java/br/com/lab2spring/ApiwebApplication.java
@@ -15,42 +15,42 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@CrossOrigin
-@EnableSwagger2
-@Controller
+//@CrossOrigin
+//@EnableSwagger2
+//@Controller
 @SpringBootApplication
 public class ApiwebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiwebApplication.class, args);
 	}
-	
-	@RequestMapping("/login")
-	public String irParaLogin() {
-		return "login";
-	}
-	
-	@RequestMapping("/index")
-	public String irParaIndex() {
-		return "index";
-	}
-	
-	@RequestMapping("/registrar")
-	public String irParaRegistro() {
-		return "registrar";
-	}
-	
-	@Bean
-	    public Docket api() {
-	        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build().apiInfo(apiInfo());
-	    }
-	    
-	    private ApiInfo apiInfo() {
-	        return new ApiInfoBuilder().title("Spring boot lab")
-	                .description("")
-	                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
-	                .contact("").license("Apache License Version 2.0")
-	                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE").version("1.0")
-	                .build();
-	    }
+//	
+//	@RequestMapping("/login")
+//	public String irParaLogin() {
+//		return "login";
+//	}
+//	
+//	@RequestMapping("/index")
+//	public String irParaIndex() {
+//		return "index";
+//	}
+//	
+//	@RequestMapping("/registrar")
+//	public String irParaRegistro() {
+//		return "registrar";
+//	}
+//	
+//	@Bean
+//	    public Docket api() {
+//	        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build().apiInfo(apiInfo());
+//	    }
+//	    
+//	    private ApiInfo apiInfo() {
+//	        return new ApiInfoBuilder().title("Spring boot lab")
+//	                .description("")
+//	                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+//	                .contact("").license("Apache License Version 2.0")
+//	                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE").version("1.0")
+//	                .build();
+//	    }
 }
